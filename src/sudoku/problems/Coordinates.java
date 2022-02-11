ac3df211c329331cdc4e1509d8168831ddcb1571
TrueJacobG <jakubgradzewicz1309@gmail.com>
2022-02-11 18:57:18 +0100

    simple intro

---
+++ src/sudoku/problems/Coordinates.java
@@ -0,0 +1,41 @@
+package sudoku.problems;
+
+import java.util.Objects;
+
+public class Coordinates {
+    private final int x;
+    private final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Coordinates that = (Coordinates) o;
+
+        return this.x == that.x && this.y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
